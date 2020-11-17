d9e3000f421cdef936e6c2f5f4cca0ad98cc1eaa
Artyom Ivanyutin <44125857+Arteamo@users.noreply.github.com>
2020-11-17 15:08:15 +0300

    CHATBACK-6 (#6)
    * CHATBACK-6: Chat room search
    
    Add search by title for rooms

--- src/main/scala/com/dolphin/db/dao/RoomDao.scala
+++ src/main/scala/com/dolphin/db/dao/RoomDao.scala
@@ -12,19 +12,26 @@ trait RoomDao {
 
   def findById(id: Int): Future[Option[Room]]
 
+  def findByTitle(title: String): Future[Seq[Room]]
+
   def list(): Future[Seq[Room]]
 }
 
 class RoomDaoImpl(db: Database)(implicit ec: ExecutionContext) extends RoomDao {
 
   override def create(room: Room): Future[Room] = {
-    val query = RoomTable += room
-    db.run(query.transactionally).map(_ => room)
+    val query = RoomTable returning RoomTable.map(_.id) into ((room, roomId) => room.copy(id = Some(roomId))) += room
+    db.run(query.transactionally)
   }
 
   override def findById(id: Int): Future[Option[Room]] = {
     val query = RoomTable.filter(_.id === id)
     db.run(query.result.headOption)
+  }
+
+  override def findByTitle(title: String): Future[Seq[Room]] = {
+    val query = RoomTable.filter(_.title.toLowerCase like s"%${title.toLowerCase}%")
+    db.run(query.result)
   }
 
   override def list(): Future[Seq[Room]] = {
