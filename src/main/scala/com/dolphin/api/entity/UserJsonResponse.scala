e63148f4778031370a9dd61acb6e3d02466c54c8
Artyom Ivanyutin <44125857+Arteamo@users.noreply.github.com>
2020-11-22 13:17:09 +0300

    CHATBACK-9: Extend last message info (#9)
    Add more of user data and message data
    Add base64 field for images in message

--- src/main/scala/com/dolphin/api/entity/UserJsonResponse.scala
+++ src/main/scala/com/dolphin/api/entity/UserJsonResponse.scala
@@ -3,7 +3,12 @@ package com.dolphin.api.entity
 import io.circe.{Decoder, Encoder}
 import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 
-case class UserJsonResponse(username: String, email: String)
+case class UserJsonResponse(
+  id: Option[Int] = None,
+  username: String,
+  email: String,
+  encodedImage: Option[String] = None
+)
 
 object UserJsonResponse {
   implicit val userJsonResponseEncoder: Encoder[UserJsonResponse] = deriveEncoder[UserJsonResponse]
