5d23a6d847204dc3356357a62e8e45f954f0c624
Artyom Ivanyutin <artem-dmitrievich1@mail.ru>
2020-11-28 16:56:29 +0300

    CHATBACK-15: Add email update

--- src/main/scala/com/dolphin/api/entity/UserUpdate.scala
+++ src/main/scala/com/dolphin/api/entity/UserUpdate.scala
@@ -3,7 +3,12 @@ package com.dolphin.api.entity
 import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 import io.circe.{Decoder, Encoder}
 
-case class UserUpdate(username: Option[String] = None, password: Option[String] = None, image: Option[ImageUpdate])
+case class UserUpdate(
+  email: Option[String] = None,
+  username: Option[String] = None,
+  password: Option[String] = None,
+  image: Option[ImageUpdate]
+)
 
 object UserUpdate {
   implicit val userUpdateEncoder: Encoder[UserUpdate] = deriveEncoder[UserUpdate]
